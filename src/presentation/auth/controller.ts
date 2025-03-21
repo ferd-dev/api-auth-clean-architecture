951848d2ec488ba2beba57c2eff5b2be5577d493
fernando <vluisfernando62@gmail.com>
2025-03-20 21:48:49 -0400

    fix: mejorar el manejo de errores en el controlador de autenticación

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express'
-import { AuthRepository, RegisterUserDto } from '../../domain'
+import { AuthRepository, CustomError, RegisterUserDto } from '../../domain'
 
 export class AuthController {
   // Aplicando de Inyeccion de Dependencias
   constructor(private readonly _authRepository: AuthRepository) {}
+
+  private _handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError)
+      return res.status(error.statusCode).json({ error: error.message })
+
+    return res.status(500).json({
+      error: 'Internal Server Error',
+    })
+  }
 
   registerUser = (req: Request, res: Response) => {
     const [error, registerUserDto] = RegisterUserDto.create(req.body)
@@ -12,9 +21,7 @@ export class AuthController {
     this._authRepository
       .register(registerUserDto!)
       .then((user) => res.json(user))
-      .catch((error) => {
-        res.status(500).json(error)
-      })
+      .catch((error) => this._handleError(error, res))
   }
 
   loginUser = (req: Request, res: Response) => {
